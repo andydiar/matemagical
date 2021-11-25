b8b1367294b8f66a4c09d3b9f8f05cfdbd9f0e0c
andydiar <andres_diar@yahoo.com>
2021-11-24 20:37:44 -0500

    cambio de prueba
    cambio de prueba

---
+++ Matemagical/Matemagical/Assets/Scripts/mouseCapture.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseCapture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        
+    }
+}
