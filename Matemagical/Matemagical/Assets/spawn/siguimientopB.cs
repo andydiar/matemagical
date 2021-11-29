c8d92ecbc23d0918414484d90afbde68935a9b2c
barrak2000 <70242607+barrak2000@users.noreply.github.com>
2021-11-29 13:14:17 -0500

    modificaciónes Spawn

---
+++ Matemagical/Matemagical/Assets/spawn/siguimientopB.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class siguimientopB : MonoBehaviour
+{
+    private Rigidbody rbEnemigo;
+    private GameObject jugador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbEnemigo = GetComponent<Rigidbody>();
+        jugador = GameObject.Find("numero1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbEnemigo.AddForce(jugador.transform.position - transform.position);
+    }
+}
---
+++ Matemagical/Matemagical/Assets/spawn/spawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform enemypos;
+    private float repeatRate = 1.0f;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            InvokeRepeating("EnemySpawner", 0.0f, repeatRate);
+            Destroy(gameObject, 11);
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void EnemySpawner()
+    {
+        Instantiate(enemy, enemypos.position, enemypos.rotation);
+    }
+}
