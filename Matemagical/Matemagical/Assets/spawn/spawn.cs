93dc4e9b158b85cd475ad4e834376d562d6a4464
barrak2000 <70242607+barrak2000@users.noreply.github.com>
2021-11-29 18:48:43 -0500

    spawn

--- Matemagical/Matemagical/Assets/spawn/spawn.cs
+++ Matemagical/Matemagical/Assets/spawn/spawn.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawn : MonoBehaviour
+public class Spawn : MonoBehaviour
 {
     public GameObject enemy;
     public Transform enemypos;
